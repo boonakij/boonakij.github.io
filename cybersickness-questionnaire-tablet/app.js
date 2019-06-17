8b95a70983af75ea0db438e7eb4b8fa372f6dcbe
boonakij <boonakij@gmail.com>
2019-06-17 14:59:03 -0500

    modify script

--- cybersickness-questionnaire-tablet/app.js
+++ cybersickness-questionnaire-tablet/app.js
@@ -6,7 +6,7 @@ function startup() {
     ui = new AppUi({
         buttonName: "SURVEY", // The name of your app
 		home: Script.resolvePath("app.html"), // The path to your app's UI
-		graphicsDirectory: Script.resolvePath("https://missouri.box.com/shared/static/h3rtbu163x2t5te6sftqdc3c7vx7drck.png") // The path to your button icons
+		graphicsDirectory: Script.resolvePath("https://boonakij.github.io/cybersickness-questionnaire-tablet/") // The path to your button icons
     });
 }
 startup();
